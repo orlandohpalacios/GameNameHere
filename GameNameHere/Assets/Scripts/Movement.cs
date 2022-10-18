7635379e704cec4b42aea1f7fe4c9b1c5738da2f
fireball5559 <ohernandezpalacios@student.neumont.edu>
2022-10-18 14:06:56 -0600

    Camera and Jump Improvements
    Made the camera follow and the jump functions with its purpose

--- GameNameHere/Assets/Scripts/Movement.cs
+++ GameNameHere/Assets/Scripts/Movement.cs
@@ -8,6 +8,7 @@ public class Movement : MonoBehaviour
     [SerializeField] public float speed;
     bool Grounded;
     private Rigidbody2D body;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -18,14 +19,27 @@ public class Movement : MonoBehaviour
     {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
 
-        if (Input.GetKey(KeyCode.UpArrow)) 
+        if (Input.GetKey(KeyCode.Space)&&Grounded) 
         {
-            body.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+            Jump();
         }
 
 /*
         if (Input.GetKey(KeyCode.UpArrow)&& Time.deltaTime<1)
             body.AddForce(Vector2.up* jumpamount,ForceMode2D.Impulse);*/
            // body.velocity = new Vector2(body.velocity.x, speed);
+    }
+    private void Jump() 
+    {
+        body.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        Grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Ground") 
+        {
+            Grounded = true;
+        }
     }
 }
